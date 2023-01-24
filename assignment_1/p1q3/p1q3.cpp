1194a11db75969672c228231d3263ffb0928b9b4
waridh <waridh@ualberta.ca>
2023-01-23 17:49:41 -0700

    working on question 3 now

---
+++ assignment_1/p1q3/p1q3.cpp
@@ -0,0 +1,22 @@
+// Assignment 1 Part 1 Question 3
+
+/*
+We are trying to split each input into a token. The tokens are delimited
+by field separators. The field separators we are going to be using are
+newline whitespace and tab. Assume that each token has at most 20 - 1
+characters. We also need to store the  number of tokens obtained in the
+variable count. Also specify a simple data structure to store the
+obtained tokens.
+
+Function required:
+  strtok()
+ 
+Arguments:
+  inStr:  The input string.
+  WSPACE: Character array of separators. "\n \t".
+  count:  The number of obtained tokens.
+*/
+
+char WSPACE[] = "\n \t";
+
+
