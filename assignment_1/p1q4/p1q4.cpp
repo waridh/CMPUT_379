ab9a30c3cb198ecf20891f6d5923c981bb2c2900
waridh <waridh@ualberta.ca>
2023-01-25 15:00:19 -0700

    some writing

---
+++ assignment_1/p1q4/p1q4.cpp
@@ -0,0 +1,9 @@
+// Assignment 1 Part 1 Question 4
+
+/*
+	When encountering an error, Many UNIX system calls return -1 and set the
+	integer errno to a value indicating the reason of the error. Give a C
+	code fragment to print on the stdout (rather than stderr) an error
+	message string corresponding to the errno value.
+*/
+
