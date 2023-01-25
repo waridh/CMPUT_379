ad5615362d7771f06bc6d9485432f1df7e3833b9
waridh <waridh@ualberta.ca>
2023-01-25 16:12:52 -0700

    done question 4

--- assignment_1/p1q4/p1q4.cpp
+++ assignment_1/p1q4/p1q4.cpp
@@ -7,3 +7,14 @@
 	message string corresponding to the errno value.
 */
 
+#include <cerrno>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+int main(int argc, char *argv[])  {
+	errno = 3;
+	printf("Error: %s\n", strerror(errno));
+	exit(0);
+}
+
