3ea2609b097a27507ddf524cfd3a86e4ee96dca2
waridh <waridh@ualberta.ca>
2023-01-26 21:52:36 -0700

    kinda completed part 2

--- assignment_1/a1p2/a1p2.cpp
+++ assignment_1/a1p2/a1p2.cpp
@@ -10,8 +10,9 @@
 int main(int argc, char *argv[])  {
 	// In this section, we are determining what the command line input that was
 	// Chosen was.
-	bool wTORsF;
+	bool wTORsF;  // This boolean is the w|s argument switch
 	if (argc < 2)  {
+		// Error handling for not having enough arguments
 		printf("Missing argument (w|s). Please run the program again.\n");
 		printf("Quitting...\n");
 		exit(0);
@@ -21,40 +22,28 @@ int main(int argc, char *argv[])  {
 		wTORsF = false;
 	};
 
-	if (wTORsF)  {
-		printf("W was chosen\n");
-	}  else  {
-		printf("S was chosen\n");
-	};
-
-	// Spawning a child, and tasking it with a job
-	//int status, pid = fork();
-	//if (pid == -1)  {
-//		perror("Child died, ending program.\n");
-//		exit(EXIT_FAILURE);
-//	}  else if (pid > 0)  {
-//		printf("This is the parent process\n");
-//		printf("The PID is: %i\n", getpid());
-//	}  else  {
-//		printf("This is a child process. Gonna run ls now.\n");
-//		status = system("ls");
-//	};
-
 	// Spawning child process to run the program
 	pid_t pid = fork();
 	if (pid > 0)  {
 		// This is the parent process
 		if (wTORsF)  {
-			//waitpid();
+			waitpid(pid, NULL, 0);
+			// Debug prints
+			// printf("The child process has killed itself\n");
+			return 0;
 		}  else  {
+			// Letting the child program run for two minutes
 			sleep(120);
-			kill(pid, SIGKILL);
-			printf("The program ran for two minutes\n");
+			kill(pid, SIGKILL);  // Kill the child process
+
+			// Debug print
+			// printf("The program ran for two minutes\n");
 			return 0;
 		}
 	}  else if (pid == 0)  {
 		// Child process. We want to run execlp.
 		execlp("./myclock", "myclock", "out1", (char *) NULL);
+		exit(EXIT_SUCCESS);
 	};
 
 	return 0;
