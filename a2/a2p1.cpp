35d739c146160b0a0fd3e93098b3bee6ca6cea22
waridh <waridh@ualberta.ca>
2023-02-13 22:10:58 -0700

    got the thread to activate

--- a2/a2p1.cpp
+++ a2/a2p1.cpp
@@ -4,6 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+//=============================================================================
+// Structures
+typedef struct  {
+  char*   inFile;
+  int     nLine;
+  int     delay;
+} super_struct;
 
 //=============================================================================
 // Error outputs
@@ -47,36 +56,81 @@ void readLine(std::ifstream &fp, int i)  {
   
 };
 
-void lineout_controller(char * inFile, int * nLine)  {
+void * lineout_controller(void * input_args)  {
   /* This function attempts to control the line output*/
   /* TODO:
         Need to figure out how we want to close the file*/
-  std::ifstream  fp(inFile);
+
+  super_struct * arguments = (super_struct *) input_args;
+  std::ifstream  fp(arguments -> inFile);
   int           i, count = 1;
-  if (fp.is_open())  {
-    for (i = 0; i < *nLine; i++)  {
-    readLine(fp, count);
-    count++;
-    }
-  };
+  std::cout << "User command:" << std::endl;
+  sleep(5);
+
+  return NULL;
 };
 
-void thread_controller(char * inFile, int * nLine, int * delay)  {
+void thread_controller(super_struct arguments)  {
+  /* This function creates and controls thread that will run the line out and
+  the command line input*/
+  pthread_t       tid1;
+  pthread_attr_t  attr1;
+  int             err;
+  int             i, count = 1;
+  std::ifstream   fp(arguments.inFile);
+  void*           tret;
+  // Setting the default attribute of the thread
+  pthread_attr_init(&attr1);
+  // Running open check
+  if (!fp.is_open())  {
+    print_file_error();
+  };
+  // Running the main loop
+  while (!fp.eof())  {
+    for (i = 0; i < arguments.nLine; i++)  {
+      if (fp.eof())  {
+        break;
+      }
+    readLine(fp, count);
+    count++;
+
+    }
+
+    std::cout << std::endl << "*** Entering a delay period of "
+    << arguments.delay << "msec" << std::endl << std::endl;
+
+    // Creating the thread
+    err = pthread_create(
+      &tid1,
+      &attr1,
+      lineout_controller,
+      (void *) &arguments);
+    if (err != 0)  {
+      std::cout << "Unable to create thread" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+    err = pthread_join(tid1, &tret);
+  };
+
+  
+  
   return;
 };
 
 int main(int argc, char * argv[])  {
-  int nLine, delay;
+  super_struct arguments;
 
   input_check(&argc);  // Running a check for correct argument amount
   disp_arg(argv);  // Disp the initial string.
   
-  char * inFile = argv[2];
-  nLine = atoi(argv[1]); delay = atoi(argv[3]);  // Turning cmd line arg to var
+  (arguments).inFile = argv[2];
+  (arguments).nLine = atoi(argv[1]); (arguments).delay = atoi(argv[3]);
+  // Turning cmd line arg to var
 
   //===========================================================================
-  // Getting the file line
-  lineout_controller(inFile, &nLine);
+  // Running the thread controller to control thread actions
+  
+  thread_controller(arguments);
 
 
   return 0;
