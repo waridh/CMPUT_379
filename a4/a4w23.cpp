ca4af92003464b9d4ea005fa11b63cdf80c65d28
Bach Wongwandanee <waridh@cs.ualberta.ca>
2023-03-21 21:56:52 -0600

    did a thing

--- a4/a4w23.cpp
+++ a4/a4w23.cpp
@@ -9,6 +9,23 @@ The final assignment for CMPUT 379
 multiple files. That seems more annoying for submission. I will try using a
 header file though.*/
 
-int  main()  {
+// Libraries
+#include <iostream>
+
+//=============================================================================
+// Error handling
+
+void  cmdline_err(int argc, char * argv[])  {
+  // Checks if the user used the command line arguments properly
+  if (argc != 4)  {
+    // Not the correct amount of command line arguments
+    std::cout << "Incorrect amount of command line arguments" << std::endl;
+    std::cout << "Quitting" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+int  main(int argc, char * argv[])  {
   // Running the main function
+
 }
