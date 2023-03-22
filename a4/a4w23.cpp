f065a22e5795b0a3fa21081c6c0e51b4b8c36cd2
Bach Wongwandanee <waridh@cs.ualberta.ca>
2023-03-22 13:31:57 -0600

    Started getting the monitoring thread working

--- a4/a4w23.cpp
+++ a4/a4w23.cpp
@@ -10,22 +10,119 @@ multiple files. That seems more annoying for submission. I will try using a
 header file though.*/
 
 // Libraries
+#include <cstring>
+#include <fcntl.h>
 #include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+
+// Global variables
+
+int                 NITER;
+
+// Critical sections
 
 //=============================================================================
 // Error handling
 
 void  cmdline_err(int argc, char * argv[])  {
   // Checks if the user used the command line arguments properly
+  size_t        i;
   if (argc != 4)  {
     // Not the correct amount of command line arguments
+    std::cout << std::endl;
     std::cout << "Incorrect amount of command line arguments" << std::endl;
     std::cout << "Quitting" << std::endl;
     exit(EXIT_FAILURE);
+  }  else  {
+    // Checking if the last and second last argument is an integer.
+    for (i = 0; i < strlen(argv[2]); i++)  {
+      // Checking through each character of the last argument to see if it digit
+      if (isdigit(argv[2][i]))  {
+        // Don't do anything
+        continue;
+      }  else  {
+        std::cout << std::endl;
+        std::cout << "The monitorTIme field is not an integer. "
+        << "Please use the proper command." << std::endl << "Quitting"
+        << std::endl;
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    for (i = 0; i < strlen(argv[3]); i++)  {
+      // Checking through each character of the last argument to see if it digit
+      if (isdigit(argv[3][i]))  {
+        // Don't do anything
+        continue;
+      }  else  {
+        std::cout << std::endl;
+        std::cout << "The last field is not an integer. Please use the proper "
+        << "command." << std::endl << "Quitting" << std::endl;
+        exit(EXIT_FAILURE);
+      }
+    }
   }
+}
+
+//=============================================================================
+// Utilities
+
+void tokenizer(char * cmdline, std::string * tokens)  {
+  /* Goal here is to tokenize the c string input*/
+  char							WSPACE[] = "\t ";
+  char *						buffer = strtok(cmdline, WSPACE);
+  int								count = 0;
+
+  while (buffer != NULL)  {
+    // Putting strings down
+    tokens[count] = buffer;
+    count++;
+    buffer = strtok(NULL, WSPACE);
+  }
+}
+
+void cmdline_eater(int argc, char * argv[], int * monitorTime)  {
+  // Collects the data from the command line arguments
+  NITER = atoi(argv[3]);  // Getting NITER
+  *monitorTime = atoi(argv[2]);  // Collecting monitor time
+
+  return;
+}
+
+//=============================================================================
+// Thread functions
+
+void * monitor_thread(void * arg)  {
+  // Creates the output every monitorTime millisecs
+  int *         monitorTime = (int *) arg;
+  int           test = 0;
+
+
+  while (1)  {
+    std::cout << "Testing, lol: " << test << std::endl;
+    usleep((*monitorTime) * 1000);
+    test++;
+  }
+  return NULL;
 }
 
 int  main(int argc, char * argv[])  {
   // Running the main function
-
+  int         monitorTime;
+  pthread_t   montid;
+  
+  cmdline_err(argc, argv);
+  cmdline_eater(argc, argv, &monitorTime);  // Process some command line args
+  
+  // Making the monitor thread
+  pthread_create(
+    &montid,
+    NULL,
+    monitor_thread,
+    (void *) &monitorTime
+  );
+  pthread_detach(montid);
+  sleep(5);
+  return 0;
 }
